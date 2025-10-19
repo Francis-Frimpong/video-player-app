39a7c86e0fa66fa33c5bc6cf8bca5d88bfc7d0f5
Francis <brakay1136@gmail.com>
2025-10-18 20:23:10 -0700

    some methods created

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 class VideoPlayer {
-  constructor() {}
+  constructor() {
+    this.video = document.getElementById("video");
+    this.playBtn = document.querySelector(".play-pause");
+    this.fullScreenBtn = document.querySelector(".fullscreen");
+  }
+
+  playVideo() {
+    const playVideo = this.video.play();
+    return playVideo;
+  }
+
+  videoFullScreen() {
+    return this.video.requestFullscreen();
+  }
+
+  addEventListeners() {
+    this.playBtn.addEventListener("click", () => this.playVideo());
+    this.fullScreenBtn.addEventListener("click", () => this.videoFullScreen());
+  }
 }
+
+const getPlayer = new VideoPlayer();
+getPlayer.addEventListeners();
