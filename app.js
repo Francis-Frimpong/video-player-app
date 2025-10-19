844be48140acba1f5cfbd56c28509df114792e61
Francis <brakay1136@gmail.com>
2025-10-19 11:36:29 -0700

    progress bar functionality done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ class VideoPlayer {
     this.video = document.getElementById("video");
     this.playBtn = document.querySelector(".play-pause");
     this.fullScreenBtn = document.querySelector(".fullscreen");
+    this.progressBar = document.querySelector(".progress-bar");
   }
 
   playVideo() {
@@ -14,9 +15,21 @@ class VideoPlayer {
     return this.video.requestFullscreen();
   }
 
+  onSliderDrag() {
+    let seekTime = (this.progressBar.value / 100) * this.video.duration;
+    this.video.currentTime = seekTime;
+  }
+
   addEventListeners() {
     this.playBtn.addEventListener("click", () => this.playVideo());
     this.fullScreenBtn.addEventListener("click", () => this.videoFullScreen());
+
+    this.video.addEventListener("timeupdate", () => {
+      this.progressBar.value =
+        (this.video.currentTime / this.video.duration) * 100;
+    });
+
+    this.progressBar.addEventListener("input", () => this.onSliderDrag());
   }
 }
 
